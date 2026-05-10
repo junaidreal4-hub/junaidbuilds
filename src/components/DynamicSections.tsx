2c1b9a10c37be03b96a03f2ea1adce92f1bf1504
Mohammed Junaid Khan <junaidreal4@gmail.com>
2026-05-10 20:33:20 +0200

    cleanup: delete 21 unused component files

--- src/components/DynamicSections.tsx
+++ src/components/DynamicSections.tsx
@@ -1,14 +1,1 @@
-'use client'
-import dynamic from 'next/dynamic'
-
-const Approach       = dynamic(() => import('@/components/Approach'),       { ssr: false })
-const PlusTransition = dynamic(() => import('@/components/PlusTransition'), { ssr: false })
-
-export default function DynamicSections() {
-  return (
-    <>
-      <Approach />
-      <PlusTransition />
-    </>
-  )
-}
+// deleted
