7ebc8e92c8beddd369615c45b738838dba540f37
Mohammed Junaid Khan <junaidreal4@gmail.com>
2026-05-10 20:33:09 +0200

    cleanup: remove all unused section components, keep Hero/Navbar/Cursor/Transition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,11 @@
-import Navbar           from '@/components/Navbar'
-import Hero             from '@/components/Hero'
-import AboutStrip       from '@/components/AboutStrip'
-import PlusTransition   from '@/components/PlusTransition'
-import Works            from '@/components/Works'
-import Services         from '@/components/Services'
-import Testimonials     from '@/components/Testimonials'
-import Contact          from '@/components/Contact'
-import Footer           from '@/components/Footer'
-import CustomCursor     from '@/components/CustomCursor'
+import Navbar from '@/components/Navbar'
+import Hero from '@/components/Hero'
 
 export default function Home() {
   return (
-    <main className="bg-canvas">
-      <CustomCursor />
+    <main>
       <Navbar />
       <Hero />
-      <AboutStrip />
-      <PlusTransition />
-      <Works />
-      <Services />
-      <Testimonials />
-      <Contact />
-      <Footer />
     </main>
   )
 }
